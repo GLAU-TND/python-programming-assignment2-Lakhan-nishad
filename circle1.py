b037435d2c505678c980e0a8a4f4042c1471a528
Lakhan-nishad <61304878+Lakhan-nishad@users.noreply.github.com>
2020-02-21 02:00:15 -0800

    Add files via upload

---
+++ circle1.py
@@ -0,0 +1,10 @@
+n = eval(input())
+a = []
+p = n[0][-1]
+a.append(n[0])
+for j in n :
+    for i in n :
+        if p == i[0] and i not in a :
+            a.append(i)
+            p = i[-1]
+print(a)
